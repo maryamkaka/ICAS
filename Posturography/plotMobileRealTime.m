70ef9df7045aeda0dde04414c6e8e9414010673d
Maryam K <mkaka139@gmail.com>
2016-11-03 10:28:00 -0400

    Wrote MATLAB script for Real Time Accel. Data collection from Android Device

---
+++ Posturography/plotMobileRealTime.m
@@ -0,0 +1,35 @@
+% connector on -> connect to MATLAB app 
+clear; clc;
+
+m = mobiledev; 
+m.AccelerationSensorEnabled = 1;
+m.logging = 1; 
+
+x = zeros(1000, 1);
+y = zeros(1000, 1);
+z = zeros(1000, 1);
+
+figure(1)
+p1 = plot(x)
+hold on
+p2 = plot(y)
+p3 = plot(z)
+legend('show')
+
+while(1)
+    [a, ~] = accellog(m);
+    if(length(a)) > 1000
+        x = a(end-999:end,1);
+        y = a(end-999:end,2);
+        z = a(end-999:end,3);
+    else 
+        x = a(:,1);
+        y = a(:,2);
+        z = a(:,3);
+    end
+    
+    p1.YData = x;
+    p2.YData = y;
+    p3.YData = z;
+    drawnow
+end 
